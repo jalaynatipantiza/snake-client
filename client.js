afc8a9af6ecf967a006ba8755ee65128541be1e0
jalayna <jalaynatipantiza@gmail.com>
2020-06-03 21:58:55 +0000

    identify our snake easily upon connection

--- client.js
+++ client.js
@@ -8,11 +8,15 @@ const connect = function() {
     port: 50541
     
   });
+  conn.setEncoding('utf8'); 
+  conn.on('connect', () => {
+    console.log("Successfully connected to game server");
+    conn.write("Name: Jay");
+  });
 
   conn.on('data', (data) => {
     console.log('Server says: ', data);
   });
-  conn.setEncoding('utf8'); 
 
   return conn;
 }
