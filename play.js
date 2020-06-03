06a7a53723182d532dcb64ed9d858a95a5a3e45f
jalayna <jalaynatipantiza@gmail.com>
2020-06-03 19:40:48 +0000

    initial client code setup

---
+++ play.js
@@ -0,0 +1,23 @@
+const net = require('net');
+
+/**
+ * Establishes connection with the game server
+ */
+const connect = function() {
+  const conn = net.createConnection({ 
+    host: 'localhost',
+    port: 50541
+  });
+  //event handler to handle incoming data and console log it for the player
+  conn.on('data', (data) => {
+    console.log('Server says: ', data);
+  });
+  // interpret incoming data as text
+  conn.setEncoding('utf8'); 
+
+  return conn;
+}
+
+console.log('Connecting ...');
+connect();
+
