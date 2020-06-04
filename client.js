dfeab1d66baca2a4fec00693c7f577912759809c
jalayna <jalaynatipantiza@gmail.com>
2020-06-04 02:55:03 +0000

    sending movement commands using connection object

--- client.js
+++ client.js
@@ -3,29 +3,23 @@ const net = require('net');
 
 
 const connect = function() {
-  const conn = net.createConnection({ 
+  const conn = net.createConnection({
     host: 'localhost',
     port: 50541
     
   });
-  conn.setEncoding('utf8'); 
+  conn.setEncoding('utf8');
   conn.on('connect', () => {
     console.log("Successfully connected to game server");
-    conn.write("Name: Jay");
-    // conn.write("Move: up");
-    // setTimeout(() => {
-    //     conn.write('Move: up');
-    // }, 1000)
-    // conn.write("Move: up")
-    // conn.write("Move: left")
   });
-
+  
+  conn.write("Name: Jay");
   conn.on('data', (data) => {
     console.log('Server says: ', data);
   });
 
   return conn;
-}
+};
 
 module.exports = {
   connect
--- play.js
+++ play.js
@@ -1,11 +1,11 @@
 
 const { connect } = require('./client');
-const { setupInput } = require('./input')
+const { setupInput } = require('./input');
 
 console.log('Connecting ...');
 
-connect();
 
-setupInput();
+
+setupInput(connect());
 
 
